08c29f858c693b21ee615e4ad6c3ea3232b970cb
sataphat denduang <sataphat.dev@gmail.com>
2020-07-04 21:28:33 +0700

    config dom pdf

--- app/Http/Controllers/LabPDFController.php
+++ app/Http/Controllers/LabPDFController.php
@@ -9,8 +9,8 @@ class LabPDFController extends Controller
     //
     public function pdf_index(){
       $data = [ ];
-      $pdf = PDF::loadView('printpdf.test_pdf',$data);
-      return $pdf->stream(rand().'.pdf'); //แบบนี้จะ stream มา preview
+       return PDF::setOptions(['isHtml5ParserEnabled' => true, 'isRemoteEnabled' => true])->loadView('printpdf.test_pdf',$data)->stream();
+      //return $pdf->stream(rand().'.pdf'); //แบบนี้จะ stream มา preview
       //return $pdf->download('test.pdf'); //แบบนี้จะดาวโหลดเลย
     }
 }
