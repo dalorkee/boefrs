55590ef48cfa38b54e24fd1bc92a930a8f7a84c9
sataphat denduang <sataphat.dev@gmail.com>
2020-07-04 22:21:24 +0700

    public_path

--- resources/views/printpdf/test_pdf.blade.php
+++ resources/views/printpdf/test_pdf.blade.php
@@ -25,25 +25,25 @@ $current_date = DateThai(date('Y-m-d'));
  font-family:  'THSarabunNew';
  font-style: normal;
  font-weight: normal;
- src: url("{{ asset('fonts/THSarabunNew.ttf') }}") format('truetype');
+ src: url("{{ public_path('fonts/THSarabunNew.ttf') }}") format('truetype');
 }
 @font-face{
  font-family:  'THSarabunNew';
  font-style: normal;
  font-weight: bold;
- src: url("{{ asset('fonts/THSarabunNew Bold.ttf') }}") format('truetype');
+ src: url("{{ public_path('fonts/THSarabunNew Bold.ttf') }}") format('truetype');
 }
 @font-face{
  font-family:  'THSarabunNew';
  font-style: italic;
  font-weight: normal;
- src: url("{{ asset('fonts/THSarabunNew Italic.ttf') }}") format('truetype');
+ src: url("{{ public_path('fonts/THSarabunNew Italic.ttf') }}") format('truetype');
 }
 @font-face{
  font-family:  'THSarabunNew';
  font-style: italic;
  font-weight: bold;
- src: url("{{ asset('fonts/THSarabunNew BoldItalic.ttf') }}") format('truetype');
+ src: url("{{ public_path('fonts/THSarabunNew BoldItalic.ttf') }}") format('truetype');
 }
 body{
  font-family: "THSarabunNew";
